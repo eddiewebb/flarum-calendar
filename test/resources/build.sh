afd35409023499d33500abc7ead10b5a7766ed79
Lier0 <28351075+Lier0@users.noreply.github.com>
2021-08-19 02:01:00 +0200

    test: Update build

--- test/resources/build.sh
+++ test/resources/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 usage (){
   echo -n "
-    $0 [ -u USER ] [ -p JS_PATH ]
+    $0 [ -u USER ] [ -p JS_PATH ] [ -c ]
+      -c does a cache:clear
   "
   exit 2;
 }
@@ -12,7 +13,12 @@ flarum_wdir="$( realpath $0 | sed "s|\(.*\)/vendor.*|\1|")" #"/var/www/flarum"
 npm_user="$flarum_user"
 npm_wdir="$flarum_wdir/vendor/webbinaro/flarum-calendar/js"
 
-while getopts 'u:p:?h' opt
+cache_clear(){
+  cd $flarum_wdir
+  sudo -u $flarum_user php flarum cache:clear
+}
+
+while getopts 'u:p:?hc' opt
 do
   case $opt in
     u) 
@@ -22,6 +28,10 @@ do
     p)
       flarum_wdir=$OPTARG
       npm_wdir="$flarum_wdir/vendor/webbinaro/flarum-calendar/js"
+      ;;
+    c)
+      cache_clear
+      exit 0
       ;;
     h|?) usage ;;
   esac
@@ -37,5 +47,4 @@ cd $npm_wdir
 cd "$npm_wdir"
 sudo -u $npm_user npm run build
 
-cd $flarum_wdir
-sudo -u $flarum_user php flarum cache:clear
+cache_clear
