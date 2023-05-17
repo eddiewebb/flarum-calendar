815fff485e386936505eb8f7611876fd56ac26e5
Rafael Horvat <rafael.horvat@glowingblue.com>
2023-05-17 14:09:39 +0200

    fix webpack config according to changes

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,18 +1,1 @@
-const config = require('flarum-webpack-config');
-
-var merge = require('webpack-merge');
-
-const myConfig = {
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-};
-
-
-
-module.exports = merge(config(), myConfig);
+module.exports = require('flarum-webpack-config')();
